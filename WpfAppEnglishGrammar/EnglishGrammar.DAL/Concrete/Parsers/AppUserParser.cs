e36e3c2fe8249b496dbe0cde087e767916c6cb27
oleghsandra <oleghsandra@gmail.com>
2016-10-19 01:25:04 +0300

    Update AppUserParser.cs

--- WpfAppEnglishGrammar/EnglishGrammar.DAL/Concrete/Parsers/AppUserParser.cs
+++ WpfAppEnglishGrammar/EnglishGrammar.DAL/Concrete/Parsers/AppUserParser.cs
@@ -6,6 +6,10 @@ namespace EnglishGrammar.DAL.Concrete.Parsers
 {
     public static class AppUserParser
     {
+        
+        //Review - Oleg: Duplication of code! You do not need so many methods to
+        //create a parser, you only need to create one parser method to create models instances.
+        
         public static AppUser SetNewUser(SqlDataReader reader)
         {
             var user = new AppUser();
