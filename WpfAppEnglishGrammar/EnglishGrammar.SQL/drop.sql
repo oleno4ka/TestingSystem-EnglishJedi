eb6b6a07b9ab97b5cb9a0abeecf0a211efdf28cd
oleghsandra <oleghsandra@gmail.com>
2016-10-21 18:48:43 +0300

    Update drop.sql

--- WpfAppEnglishGrammar/EnglishGrammar.SQL/drop.sql
+++ WpfAppEnglishGrammar/EnglishGrammar.SQL/drop.sql
@@ -1,6 +1,8 @@
 ﻿USE EnglishJedi;
 GO
 
+--Review - Oleg Shandra: Error during script execution because it does not consider foreign keys in tables.
+--It is necessary to drop the tables in reverse order of their creation.
 DROP TABLE Answer;
 DROP TABLE Question;
 DROP TABLE Mark;
